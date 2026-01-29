ecac1af2d36b506f821af26532b237e39f101d72
Mina <mina_daoud@outlook.com>
2026-01-29 20:54:24 +0200

    code sharing and reuse between services

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@minadaoud11/common";
 
 const router = express.Router();
 
